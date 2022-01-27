c041ea51329709111b445d44285d14488be5b822
FrienDDDD <friendjunk@gmail.com>
2022-01-27 21:17:24 +0700

    \test

---
+++ hi.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include<cstdlib>
+using namespace std;
+
+int main(int argc,char *argv[])
+{
+    double sum = 0,average ;
+    if(argc == 1){
+        cout << "Please input numbers to find average." ;
+    }else{
+        cout << "---------------------------------" << endl ;
+        for(int i = 1; i < argc;i++){
+            sum += atof(argv[i]) ;
+        }
+        average = sum/(argc-1) ;
+        cout << "Average of " << argc-1 <<  " numbers = " << average << endl ;
+        cout << "---------------------------------" ;
+    }
+    
+    
+    return 0;
+}
